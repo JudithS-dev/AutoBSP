6973d38e477acf41ad92703baf7b4d8f92a207c0
Judith <judith.schmidt.lth@t-online.de>
2025-12-03 07:42:51 +0100

    feat(lexer): add helper functions to suggest closest keyword for unknown tokens
    - Added `lexer_helper.c` and `lexer_helper.h` implementing `find_closest_keyword` and `levenshtein_distance`.
    - Updated `lexerDefinition.l` to propose the closest matching keyword when an unknown token is encountered.
    - Updated `Makefile` to include and compile the new helper files.
    - Updated `testBSP` to include a mismatched keyword for testing the suggestion mechanism.

---
+++ generator/lexer_helper.c
@@ -0,0 +1,125 @@
+#include "lexer_helper.h"
+
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
+/**
+ * @brief list of all keywords recognized by the lexer
+ * @note used for finding closest matching keyword
+ * @note must be kept in sync with the keywords defined in the lexer
+ */
+static const char *lexer_keywords[] = {
+  "AutoBSP",
+  "Controller",
+  "OUTPUT",
+  "INPUT",
+  "name",
+  "pin",
+  "type",
+  "pull",
+  "speed",
+  "init",
+  "active",
+  "enable",
+  "true",
+  "false",
+  "high",
+  "low",
+  "STM32F446RE",
+  "pushpull",
+  "opendrain",
+  "up",
+  "down",
+  "none",
+  "medium",
+  "very_high",
+  "on",
+  "off"
+};
+
+// number of keywords in the lexer_keywords array
+static const int lexer_keywords_count = sizeof(lexer_keywords) / sizeof(lexer_keywords[0]);
+
+/**
+ * @brief returns the minimum of three integer values
+ * 
+ * @param a first integer
+ * @param b second integer
+ * @param c third integer
+ * @return int minimum value
+ */
+int min_3_values(int a, int b, int c){
+  if(a < b)
+    return (a < c) ? a : c;
+  else
+    return (b < c) ? b : c;
+}
+
+/**
+ * @brief calulates the levenshtein distance between two strings (s1 and s2)
+ * 
+ * @param s1 first string
+ * @param s2 second string
+ * @return unsigned int levenshtein distance
+ * 
+ * @note source: https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#C
+ * 
+ * The levenshtein distance is a measure of the difference between two strings.
+ * It is defined as the minimum number of single-character edits (insertions, deletions or substitutions)
+ * required to change one string (s1) into the other (s2).
+ * Is commutative, i.e., distance(s1, s2) == distance(s2, s1)
+ */
+unsigned int levenshtein_distance(const char *s1, const char *s2){
+  unsigned int len1, len2;
+  len1 = strlen(s1);
+  len2 = strlen(s2);
+  
+  unsigned int matrix[len2 + 1][len1 + 1];
+  unsigned int i, j;
+  for(i = 0; i <= len2; i++) matrix[i][0] = i;
+  for(j = 0; j <= len1; j++) matrix[0][j] = j;
+  
+  for(i = 1; i <= len2; i++){
+    for(j = 1; j <= len1; j++){
+      // check if characters are the same
+      unsigned int cost = (s1[j - 1] == s2[i - 1]) ? 0 : 1;
+      // calculate next matrix cell value
+      matrix[i][j] = min_3_values(
+        matrix[i - 1][j] + 1,
+        matrix[i][j - 1] + 1,
+        matrix[i - 1][j - 1] + cost
+      );
+    }
+  }
+  return matrix[len2][len1];
+}
+
+/**
+ * @brief finds the closest matching keyword to the given string
+ * 
+ * @param string input string
+ * @return const char* closest matching keyword or NULL if no close match found
+ * 
+ * @note uses the levenshtein distance to find the closest match
+ */
+const char* find_closest_keyword(const char* string){
+  unsigned int cur_best_distance = UINT_MAX;
+  const char* cur_keyword = NULL;
+  
+  // iterate through all keywords to find the closest match
+  for(int i = 0; i < lexer_keywords_count; i++){
+    unsigned int distance = levenshtein_distance(string, lexer_keywords[i]);
+    if(distance < cur_best_distance){
+      cur_best_distance = distance;
+      cur_keyword = lexer_keywords[i];
+    }
+  }
+  
+  // only return a keyword if the distance is small enough (max 3 edits)
+  if(cur_best_distance <= 3){
+    return cur_keyword;
+  } else {
+    return NULL;
+  }
+}
---
+++ generator/lexer_helper.h
@@ -0,0 +1,6 @@
+#ifndef __LEXER_HELPER_H__
+#define __LEXER_HELPER_H__
+
+const char* find_closest_keyword(const char* string);
+
+#endif // __LEXER_HELPER_H__
