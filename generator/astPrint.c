a765d2fc095f854ff2d430166eba344be049e233
Judith <judith.schmidt.lth@t-online.de>
2025-12-16 15:26:34 +0100

    feat(ast): improve DOT formatting for clearer AST visualization
    - Updated `astPrint.c` to enhance DOT output with bullet points and colors for improved readability and visualization.

--- generator/astPrint.c
+++ generator/astPrint.c
@@ -59,42 +59,68 @@ void ast_print(const dsl_node_t* dsl_node){
  */
 static void ast_print_helper(FILE *pfDot, const dsl_node_t* dsl_node){
   fprintf(pfDot, "digraph AST {\n");
-  fprintf(pfDot, "  node [shape=box];\n");
+  fprintf(pfDot, "  splines=ortho;\n");
+  fprintf(pfDot, "  node [shape=plain, fontname=\"Helvetica\"];\n");
+  fprintf(pfDot, "  edge [arrowhead=vee];\n");
   
   // Print controller node
-  fprintf(pfDot, "  Controller [label=\"Controller: %s\"];\n", controller_to_string(dsl_node->controller));
+  fprintf(pfDot,
+  "  Controller [label=<<TABLE BORDER=\"1\" CELLBORDER=\"0\" CELLSPACING=\"0\" CELLPADDING=\"8\" BGCOLOR=\"#E8E8E8\">"
+  "<TR><TD><B>Controller</B></TD></TR><TR><TD ALIGN=\"LEFT\">%s</TD></TR></TABLE>>];\n",
+  controller_to_string(dsl_node->controller)
+  );
   
   // Print modules
   module_node_t *current_module = dsl_node->modules_root;
   while(current_module != NULL){
+    // Determine background color based on module kind and enable status
+    const char *colour = "#FFFFFF";
+    if(current_module->kind == MODULE_OUTPUT)
+      colour = current_module->enable ? "#B7D9F7" : "#EEF6FD";
+    else // MODULE_INPUT
+      colour = current_module->enable ? "#C6EFC6" : "#F2FBF2";
+    
+    
     // Print general module info
     char *pin_str = pin_to_string(current_module->pin);
     
-    fprintf(pfDot, "  Module%d [label=\"", current_module->node_id);
-    fprintf(pfDot, "Module ID: %d\\n",  current_module->node_id);
-    fprintf(pfDot, "Name: %s\\n",       current_module->name);
-    fprintf(pfDot, "Pin: %s\\n",        pin_str);
-    fprintf(pfDot, "Enable: %s\\n",     bool_to_string(current_module->enable));
-    fprintf(pfDot, "Kind: %s",          kind_to_string(current_module->kind));
+    fprintf(pfDot, "  Module%d [label=<<TABLE BORDER=\"1\" CELLBORDER=\"0\" CELLSPACING=\"0\" CELLPADDING=\"6\" BGCOLOR=\"%s\">", 
+                      current_module->node_id, colour);
+    
+    // Module name as header
+    fprintf(pfDot, "<TR><TD><B>%s</B></TD></TR>", current_module->name);
+    
+    // General module attributes as bullet points
+    fprintf(pfDot, "<TR><TD ALIGN=\"LEFT\">&#8226; <B>ID:</B> %d</TD></TR>",     current_module->node_id);
+    fprintf(pfDot, "<TR><TD ALIGN=\"LEFT\">&#8226; <B>Pin:</B> %s</TD></TR>",    pin_str);
+    fprintf(pfDot, "<TR><TD ALIGN=\"LEFT\">&#8226; <B>Enable:</B> %s</TD></TR>", bool_to_string(current_module->enable));
+    fprintf(pfDot, "<TR><TD ALIGN=\"LEFT\">&#8226; <B>Kind:</B> %s</TD></TR>",   kind_to_string(current_module->kind));
     
     free(pin_str);
     
     // Print module-specific data
     switch(current_module->kind){
-      case MODULE_OUTPUT: fprintf(pfDot, "\\nType: %s",        gpio_type_to_string(current_module->data.output.type));
-                          fprintf(pfDot, "\\nPull: %s",        gpio_pull_to_string(current_module->data.output.pull));
-                          fprintf(pfDot, "\\nSpeed: %s",       gpio_speed_to_string(current_module->data.output.speed));
-                          fprintf(pfDot, "\\nInit: %s",        gpio_init_to_string(current_module->data.output.init));
-                          fprintf(pfDot, "\\nActive Level: %s",level_to_string(current_module->data.output.active_level));
+      case MODULE_OUTPUT: fprintf(pfDot, "<TR><TD ALIGN=\"LEFT\">&#8226; <B>Type:</B> %s</TD></TR>",
+                                          gpio_type_to_string(current_module->data.output.type));
+                          fprintf(pfDot, "<TR><TD ALIGN=\"LEFT\">&#8226; <B>Pull:</B> %s</TD></TR>",
+                                          gpio_pull_to_string(current_module->data.output.pull));
+                          fprintf(pfDot, "<TR><TD ALIGN=\"LEFT\">&#8226; <B>Speed:</B> %s</TD></TR>",
+                                          gpio_speed_to_string(current_module->data.output.speed));
+                          fprintf(pfDot, "<TR><TD ALIGN=\"LEFT\">&#8226; <B>Init:</B> %s</TD></TR>",
+                                          gpio_init_to_string(current_module->data.output.init));
+                          fprintf(pfDot, "<TR><TD ALIGN=\"LEFT\">&#8226; <B>Active Level:</B> %s</TD></TR>",
+                                          level_to_string(current_module->data.output.active_level));
                           break;
-      case MODULE_INPUT:  fprintf(pfDot, "\\nPull: %s",         gpio_pull_to_string(current_module->data.input.pull));
-                          fprintf(pfDot, "\\nActive Level: %s", level_to_string(current_module->data.input.active_level));
+      case MODULE_INPUT:  fprintf(pfDot, "<TR><TD ALIGN=\"LEFT\">&#8226; <B>Pull:</B> %s</TD></TR>",
+                                          gpio_pull_to_string(current_module->data.input.pull));
+                          fprintf(pfDot, "<TR><TD ALIGN=\"LEFT\">&#8226; <B>Active Level:</B> %s</TD></TR>",
+                                          level_to_string(current_module->data.input.active_level));
                           break;
       default:  log_error("ast_print_helper", 0, "Unknown module kind enum value '%d'", current_module->kind);
                 break; // This won't be reached due to log_error exiting
     }
     
-    fprintf(pfDot, "\"];\n");
+    fprintf(pfDot, "</TABLE>>];\n");
     current_module = current_module->next;
   }
   
